f8feed1fa5bab35062b52ff788699f33fa19d05e
DeroMal <derrickmal123@gmail.com>
2023-08-23 23:42:37 +0200

    actor code and implement suggested improvements

--- Cloudflare Workers/workers.js
+++ Cloudflare Workers/workers.js
@@ -2,39 +2,53 @@ addEventListener("fetch", (event) => {
     event.respondWith(handleRequest(event.request));
   });
   
-  const GAS_URL = "https://script.google.com/macros/s/AKfycbxDTpPqWR520vV95XqSflaoGqWOF0Dv2ReDx-cD8WqedHTVwJg0dWMxkxftav30XkDU/exec";
+  const GAS_URL = "https://script.google.com/macros/s/AKfycbwc6ogM0tIJfEkvWh_m8ROUmOorQdaxHUwO_TBdcOjyU64rKCenhhGUfBWxuCvbiPCG/exec";
   
   async function handleRequest(request) {
     if (request.method === "OPTIONS") {
-      // Handle CORS preflight request.
-      return new Response(null, {
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "POST, OPTIONS",
-          "Access-Control-Allow-Headers": "Content-Type",
-        },
-      });
+        // Handle CORS preflight request.
+        return new Response(null, {
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "POST, OPTIONS",
+                "Access-Control-Allow-Headers": "Content-Type",
+            },
+        });
     } else if (request.method === "POST") {
-      // Read the body into a buffer
-      const body = await request.text();
-      
-      // Forward the request to the Google Apps Script
-      const response = await fetch(GAS_URL, {
-        method: "POST",
-        body: body,
-        headers: { "Content-Type": "application/json" },
-      });
+        try {
+            const body = await request.text();
+            const response = await fetch(GAS_URL, {
+                method: "POST",
+                body: body,
+                headers: { "Content-Type": "application/json" },
+            });
   
-      const results = await response.json();
+            if (!response.ok) {
+                throw new Error(`Failed to fetch from Google Apps Script. Status: ${response.status}`);
+            }
   
-      // Respond to the original caller with the results from the Google Apps Script
-      return new Response(JSON.stringify(results), {
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-        },
-      });
+            const results = await response.json();
+            return new Response(JSON.stringify(results), {
+                headers: {
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "*",
+                },
+            });
+        } catch (err) {
+            console.error(err);  // Log the error for debugging
+            return new Response(JSON.stringify({
+                status: "error",
+                message: err.message || "An unexpected error occurred",
+                top5: []
+            }), {
+                status: 500,
+                headers: {
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "*",
+                }
+            });
+        }
     } else {
-      return new Response("Method not allowed", { status: 405 });
+        return new Response("Method not allowed", { status: 405 });
     }
   }
